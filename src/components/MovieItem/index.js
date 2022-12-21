a84b8ab3a9b62c147ca1c324e2d20528d06b903a
sowmya2408 <sowmyamedaboina2000@gmail.com>
2022-12-21 16:30:16 +0000

    solution

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -25,7 +25,7 @@ const MovieItem = props => {
             <button
               className="close-button"
               type="button"
-              id="closeButton"
+              testid="closeButton"
               onClick={() => close()}
             >
               <IoMdClose size={20} color="#231f20" />
